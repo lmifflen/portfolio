207040cfaf5a84534a79bd46a7e212ec19274312
lmifflen <lmifflen@gmail.com>
2022-08-16 15:41:28 -0600

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Introduction from './components/Introduction';
 
 function App() {
   return (
     <div className="App">
      Luke's portfolio website
+     <BrowserRouter>
+     <Routes>
+     <Introduction path="/" />
+      </Routes>
+     </BrowserRouter>
+  
     </div>
   );
 }
