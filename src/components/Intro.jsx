38cb81240bef28cb798bcdd49f1ade7a24b4b498
lmifflen <lmifflen@gmail.com>
2022-08-21 11:50:09 -0600

    updating image

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import './Intro.css'
+import React from "react";
+// import "./Intro.css";
+import "./intro.scss";
 
 const Intro = () => {
   return (
-    <div className='container'>
-        <div className='right'>
-            <h1> HELLO <br></br>I'M LUKE </h1>
-            </div>
-        
-        </div>
-  )
-}
+    <div className="container">
+      <div className="right">
+        <h1>
+          {" "}
+          HELLO <br></br>I'M LUKE{" "}
+        </h1>
+      </div>
+      <div className="left">
+        <div className="imageContainer">
+      
+<img src="./luketest.svg" alt="Luke" />
+<div className="imageOverlay"></div>
 
-export default Intro
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
