f2dc4738c91ec7bfffb60b0622861dd74618716a
lmifflen <lmifflen@gmail.com>
2022-08-22 17:39:43 -0600

    building portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Portfolio = () => {
+    const [currentSlider, setCurrentSlider] = useState(0);
   const data = [
     {
       id: "1",
@@ -9,7 +10,7 @@ const Portfolio = () => {
             Socket.io, Passport, Trello`,
       roles: " Project Manager  Full Stack Developer Scrum Master",
 
-      img: "add image here",
+      img: "./Wink.PNG",
       link: `https://www.youtube.com/watch?v=Nv7JzOsvkq4`,
     },
     {
@@ -17,19 +18,58 @@ const Portfolio = () => {
       title: "A Full Stack Web Application Built Using:",
       desc: `JavaScript, ReactJS, NodeJS, ExpressJS, REST API, Git, MongoDB,
       Mapbox, Auth0, Trello, Heroku`,
-        roles: " Project Manager  Full Stack Developer Scrum Master",
-      img: "add image here",
+      roles: " Project Manager  Full Stack Developer Scrum Master",
+      img: "./Cycle.PNG",
       link: `https://city-cycle-client.herokuapp.com/`,
     },
     {
       id: "3",
       title: "My Github",
       desc: "Check out my latest work on Github!",
-      img: "add image here",
+      img: "./Git.PNG",
       link: `https://github.com/lmifflen`,
     },
   ];
-  return <div>Portfolio</div>;
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
+
+  const handleClick = (event) => {
+    event === "left"
+      ? setCurrentSlider(currentSlider > 0 ? currentSlider - 1 : 1)
+      : setCurrentSlider(
+          currentSlider < data.length - 1 ? currentSlider + 1 : 0
+        );
+  }; //If click left and current slider is less than 0, then move to #2. Else, minus 1. Else (right click) if current slider is less than data.length, +1. Else, = 0 (max, go back to 0)
+
+  return (
+    <div className="profile" id="profile">
+      <div className="card">
+        {data.map((data) => (
+          <div className="card-container" key={data.id}>
+            <div className="item" onClick={() => openInNewTab(data.link)}>
+              <div className="imageContainer">
+                <img src={data.img} alt={""} id={data.id} />
+              </div>
+              <div>
+                <div className="textContainer">
+                  <div> Projects </div>
+                  <div> {data.title} </div>
+                  <div> {data.desc} </div>
+                  <div> Roles </div>
+                  <div> {data.roles} </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img src="angle-left-solid.svg" className="arrow left" alt=""  onClick={()=>handleClick("left")}/>
+        <img src="angle-right-solid.svg" className="arrow right" alt="" onClick={()=>handleClick("right")}/>
+    </div>
+  );
 };
 
 export default Portfolio;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../components/About/About'
 import Intro from '../components/Intro/Intro'
+import Portfolio from '../components/Portfolio/Portfolio'
 
 const Home = () => {
   return (
     <div>
     <Intro></Intro>
     <About></About>
+    <Portfolio></Portfolio>
     </div>
   )
 }
