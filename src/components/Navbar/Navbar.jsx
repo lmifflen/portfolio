c863e3fec4d0d11cd938555fc3d67cb625163c62
lmifflen <lmifflen@gmail.com>
2022-08-30 15:09:44 -0600

    built hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Contact from "../Contact/Contact";
 import ChatPopup from "../Contact/ContactPopup";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   // const [contact, setContact] = React.useState(false);
   return (
     <div className="navbar">
       <div className="logo">
         <img src="./Logo.png" alt="logo" />
       </div>
-      <ul className="nav-list">
+      <div className="navlist-container">
+      <ul className={click ? "nav-menu active" : "nav-list"}>
         <li>
           <a href="#home">Home</a>
         </li>
@@ -28,6 +31,22 @@ const Navbar = () => {
         </li>
         
       </ul>
+      </div>
+      <div
+              className="hamburger"
+              onClick={handleClick}
+              style={{
+                marginBottom: "1rem",
+              }}
+            >
+              <img src="./hamburger.svg" alt="hamburger" />
+                {/* {click ? (
+                  <BiMenuAltLeft className="icon" size={50} />
+                ) : (
+                  <BiMenu className="icon" size={50} />
+                )}
+              </Badge> */}
+            </div>
     </div>
   );
 };
