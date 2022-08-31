5081d427fe630f04987ecf96cef9f64429efef73
lmifflen <lmifflen@gmail.com>
2022-08-31 11:29:32 -0600

    updating navbar mobile css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,19 @@ const Navbar = () => {
   // const [contact, setContact] = React.useState(false);
   return (
     <div className="navbar">
+      <div className="holder">
       <div className="logo">
         <img src="./Logo.png" alt="logo" />
       </div>
+      <div className="contact-button"> <ChatPopup></ChatPopup>
+           
+          </div>
+          </div>
       <div className="navlist-container">
+     
+          
+          
+        
       <ul className={click ? "nav-menu active" : "nav-list"}>
         <li>
           <a href="#home" onClick={handleClick}>Home</a>
@@ -23,12 +32,7 @@ const Navbar = () => {
         <li>
           <a href="#projects" onClick={handleClick}>Projects</a>
         </li>
-        <li>
-          <div className="contact-button" onClick={handleClick}> <ChatPopup></ChatPopup>
-           
-          </div>
-          
-        </li>
+        
         
       </ul>
       </div>
