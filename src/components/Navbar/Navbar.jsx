4b8d553e6b73534f8c5bef34738bb7b808d60858
lmifflen <lmifflen@gmail.com>
2022-09-27 16:44:16 -0600

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="holder">
       <div className="logo">
-        <img src="./Logo.png" alt="Logo" />
+        <img src="./Logo.PNG" alt="Logo" />
       </div>
      
           </div>
